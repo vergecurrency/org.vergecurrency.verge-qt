c0bd0e6a9ba84c24825f6519247c3c57c99fd0b9
sunerok <justinvforvendetta@gmail.com>
2026-09-02 18:54:24 -0400

    Update Verge package to v26.8 AppImages

--- install_wrappers.sh
+++ install_wrappers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-for b in verge{d,-qt,-cli}
+for b in verge{d,-qt,-cli,-tx}
 do
   echo '#!/usr/bin/bash' > /app/bin/$b
-  echo "exec /app/bin/internal/$b" '-datadir="${XDG_DATA_HOME}" "$@"' >> /app/bin/$b
+  echo "exec /app/lib/verge/$b.AppDir/AppRun" '-datadir="${XDG_DATA_HOME}" "$@"' >> /app/bin/$b
   chmod 744 /app/bin/$b
 done
